80b3d32af43e553fea321689208e948820032415
Michael Gygli <gygli@vision.ee.ethz.ch>
2016-03-03 14:44:02 +0100

    some additional comments
    add test_videos dir

--- evaluation/evaluation.py
+++ evaluation/evaluation.py
@@ -1,33 +1,51 @@
-from sklearn.metrics import precision_recall_curve
-from sklearn.metrics import auc
-import numpy as np
-import sys
+'''
+This module contains the evaluation metrics used in
+ Michael Gygli, Yale Song, Liangliang Cao
+    "Video2GIF: Automatic Generation of Animated GIFs from Video," IEEE CVPR 2016
 
+ The metrics are
+  - Average Precision (AP)
+  - normalized Meaningful Summary Duration (nMSD)
+
+  Furthermore it has the helper function get_gt_score, which builds the ground truth vector using
+  the dataset (pandas.DataFrame object)
+'''
+__author__ = 'michaelgygli'
+from sklearn.metrics import precision_recall_curve
+import numpy as np
+import pandas as pd
 
 
 def get_gt_score(youtube_id,dataset):
     '''
      get the ground truth vector for all the gifs of that video
-    :param youtube_id:
-    :return: ground_truth_selection
+    :param youtube_id: a youtube_id in from the dataset
+    :return: ground_truth_selection: a list of label vectors
     '''
+    assert isinstance(dataset,pd.DataFrame), "dataset needs to be a pandas DataFrame"
+
+    # Get the rows for this Youtube ID
     rows=dataset[dataset.youtube_id == youtube_id]
+
+    # Get the position of the aligned GIFs
     all_start_end_frames=rows[['gif_start_frame','gif_end_frame']].get_values()
+
+    # For each GIF that was created from this video, create an indicator vector
     ground_truth_selection=[]
     for start_frame,end_frame in all_start_end_frames:
         y_gt=np.zeros(int(rows['video_frame_count'].get_values()[0]))
-        y_gt[int(np.round(float(start_frame))):int(np.round(float(end_frame)))]=1
+        y_gt[int(start_frame):int(end_frame)]=1
         ground_truth_selection.append(y_gt)
 
     return ground_truth_selection
 
 
-def get_ap(y_true, y_predict, interpolate=True,point_11=True):
+def get_ap(y_true, y_predict, interpolate=False,point_11=False):
     '''
     Average precision in different formats: (non-) interpolated and/or 11-point approximated
     point_11=True and interpolate=True corresponds to the 11-point interpolated AP used in
     the PASCAL VOC challenge up to the 2008 edition and has been verfied against the vlfeat implementation
-    Also, the exact average precision corresponds to the one of vl_feat
+    The exact average precision (interpolate=False, point_11=False) corresponds to the one of vl_feat
 
     :param y_true: list/ numpy vector of true labels in {0,1} for each element
     :param y_predict: predicted score for each element
@@ -35,54 +53,78 @@ def get_ap(y_true, y_predict, interpolate=True,point_11=True):
     :param point_11: Use 11-point approximation to average precision?
     :return: average precision
     '''
+
+    # Check inputs
+    assert len(y_true)==len(y_predict), "Prediction and ground truth need to be of the same length"
     if len(set(y_true))==1:
         if y_true[0]==0:
             raise ValueError('True labels cannot all be zero')
         else:
             return 1
+    else:
+        assert sorted(set(y_true))==[0,1], "Ground truth can only contain elements {0,1}"
+
+    # Compute precision and recall
     precision, recall, _ = precision_recall_curve(y_true, y_predict)
     recall=recall.astype(np.float32)
 
-    if interpolate:
+    if interpolate: # Compute the interpolated precision
         for i in range(1,len(precision)):
             precision[i]=max(precision[i-1],precision[i])
 
-    if point_11:
+    if point_11: # Compute the 11-point approximated AP
          precision_11=[precision[np.where(recall>=t)[0][-1]] for t in np.arange(0,1.01,0.1)]
          return np.mean(precision_11)
-    else:
+    else: # Compute the AP using precision at every additionally recalled sample
         indices=np.where(np.diff(recall))
         return np.mean(precision[indices])
 
 
 
 
-def meaningful_summary_duration(gif_ground_truths,scores,min_inclusion=0.5,mode='mean'):
+def meaningful_summary_duration(gif_ground_truths,y_predict, min_inclusion=0.5,mode='mean'):
     '''
     Compute the meaningful summary duration, inspired by "Category-specific video summarization"
     We however normalize by the length of the gif and the length of the video
-    :param gif_ground_truths:
-    :param scores:
+    :param gif_ground_truths: a list of label vectors, one for each GIF of a particular video
+    :param y_predict: the predicted score for each frame of the video
     :param min_inclusion: portion of the ground truth GIF that eeds to be inluded
     :param mode: Can be 'mean' (average nMSD) or 'best' (best/minimal nMSD when there are multiple GIFs for a video)
     :return: msd_score
     '''
 
-    # Account for the fact, that some part of the gif might not be included in a segment at all!
-    # Only enforce overlap of min_inclusion
+    # Check inputs
+    assert isinstance(gif_ground_truths, list), "gif_ground_truths needs to be a list"
+    for y_true in gif_ground_truths:
+        assert len(y_true)==len(y_predict), "Prediction and ground truth need to be of the same length"
+        if len(set(y_true))==1:
+            raise ValueError('True labels cannot all be the same')
+        else:
+            assert sorted(set(y_true))==[0,1], "Ground truth can only contain elements {0,1}"
+
+
+    # Compute MSD w.r.t. each ground truth GIF
     msd_scores=[]
-    for y_gt in gif_ground_truths:
-        gif_scores=scores[np.where(y_gt>0)[0]]
+    for y_true in gif_ground_truths:
+
+        gif_scores=y_predict[np.where(y_true>0)[0]]
+
+        # Account for the fact, that some part of the gif might not be included in a segment at all!
+        # Only enforce overlap of min_inclusion
         score_threshold=np.percentile(gif_scores,100*(1-min_inclusion))
-        meaningful_duration=np.sum(scores>=score_threshold)
 
+        # Compute plain MSD
+        meaningful_duration=np.sum(y_predict>=score_threshold)
 
-        gif_duration = float(len(np.nonzero(y_gt)[0]))
-        video_duration = float(len(scores))
-        final_inclusion=np.sum(y_gt[scores>=score_threshold])/gif_duration
+        # Get normalization parameters
+        video_duration = float(len(y_predict))
+
+        # How much of the ground truth GIF was actually included
+        final_inclusion=np.sum(y_true[y_predict>=score_threshold])
+        assert final_inclusion>=min_inclusion*len(gif_scores), "Less than min_inclusing was in the selection"
 
         # Compute normalized msd
-        norm_msd = (meaningful_duration-gif_duration*final_inclusion) / (video_duration-gif_duration*final_inclusion)
+        norm_msd = (meaningful_duration-final_inclusion) / (video_duration-final_inclusion)
 
         msd_scores.append(norm_msd)
 
---
+++ example.py
@@ -0,0 +1,45 @@
+'''
+ This example shows how to load the dataset using python and how to evaluate a method
+ For each video in the test set it
+  - randomly scores frames
+  - computes the average precision and nMSD
+'''
+__author__ = 'michaelgygli'
+import pandas as pd
+import numpy as np
+import sys
+
+
+# Add evaluation to path and import it
+sys.path.insert(0,'./evaluation/')
+import evaluation
+
+
+# Read csv file using pandas
+#For more info on pandas check http://pandas.pydata.org/pandas-docs/stable/10min.html
+dataset=pd.read_csv('metadata.txt',sep=';\t',engine='python')
+
+# Read test IDs
+with open('testset.txt','r') as f:
+    test_ids=[l.rstrip('\n') for l in f]
+
+
+def evaluate_random():
+    '''
+     This function shows how a method can be evaluated
+    '''
+    all_ap=np.zeros(len(test_ids))
+    all_msd=np.zeros(len(test_ids))
+    for idx,youtube_id in enumerate(test_ids):
+        y_gt=evaluation.get_gt_score(youtube_id,dataset)
+        y_predicted=np.random.rand(len(y_gt[0]))
+
+        all_ap[idx] = evaluation.get_ap(np.array(y_gt).max(axis=0), y_predicted)
+        all_msd[idx] = evaluation.meaningful_summary_duration(y_gt,y_predicted)
+    print('AP=%.2f%%; MSD=%.2f%%' % (100*np.mean(all_ap),100*np.mean(all_msd)))
+
+
+if __name__=='__main__':
+    sys.stdout.write('Evaluate random performance\n')
+    sys.stdout.flush()
+    evaluate_random()
